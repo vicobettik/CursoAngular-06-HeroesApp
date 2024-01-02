2ad8ece0099178b8095b096b1faaa18989600c5a
Victor Fuentes <vicuo@hotmail.com>
2024-01-02 10:54:29 -0600

    se crean rutas hijas con layouts

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { registerLocaleData } from '@angular/common';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes:Routes = [];
+const routes:Routes = [
+    {
+        path: '',
+        component: LayoutPageComponent,
+        children: [
+            { path: 'login', component: LoginPageComponent },
+            { path: 'new-account', component: RegisterPageComponent },
+            { path: '**', redirectTo: 'login' },
+          ]
+    }
+];
 
 
 @NgModule({
