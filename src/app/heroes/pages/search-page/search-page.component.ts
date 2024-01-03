d33b3fac53c8fca2af82efacf6ba03838c291152
Victor Fuentes <vicuo@hotmail.com>
2024-01-03 10:45:24 -0600

    se termina autocomplete al buscar un heroe

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { every } from 'rxjs';
+import { MatFabButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-search-page',
@@ -9,5 +14,29 @@ import { FormControl } from '@angular/forms';
 export class SearchPageComponent {
 
   public searchInput = new FormControl('');
+
+  public heroes:Hero[] = [];
+  public selectedHero?:Hero;
+
+  constructor(private heroesService:HeroesService){}
+
+  searchHero(){
+    const value:string = this.searchInput.value || '';
+    
+    this.heroesService.getSuggestions(value)
+      .subscribe((heroes) => {
+        this.heroes = heroes
+      })
+  }
+
+  onSelectedOption(event:MatAutocompleteSelectedEvent):void{
+    if (!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+    const hero:Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
+  }
 
 }
